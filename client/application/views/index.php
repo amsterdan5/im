02d21dfe9861f4dbfe6854f4f166d50a28ed9d20
amsterdan <amsterdan@163.com>
2016-09-06 23:00:09 +0800

    客户端

---
+++ client/application/views/index.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>title</title>
+        <script src="http://hu.im.com/public/scripts/socket.io.js"></script>
+        <scritp >
+            
+        </scritp>
+    </head>
+    <body>
+    </body>
+</html>
--- server/config.php
+++ server/config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'SERVER_IP' => '192.168.1.3',//服务器ip
+    'SERVER_IP' => '192.168.1.5',//服务器ip
     'SERVER_PORT' => '9555',//服务器端口
 
     'ENABLE_CACHE' => true,//开启缓存 true 开启 false 关闭
